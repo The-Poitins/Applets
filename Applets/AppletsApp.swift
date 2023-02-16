5c9656ec6b4492f710348938808427bea39b5f42
egrishina <egrishina22@fed.idserve.net>
2023-02-16 10:13:37 +0100

    Initial Commit

---
+++ Applets/AppletsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppletsApp.swift
+//  Applets
+//
+//  Created by Ekaterina Grishina on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AppletsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

37b136c028252c1d711e7f138abe56eb20afdda0
alexnikxx <alexnikxx@users.noreply.github.com>
2023-02-20 12:16:19 +0100

    made cell

--- Applets/UI/MainScreen/ContentViewModel.swift
+++ Applets/UI/MainScreen/ContentViewModel.swift
@@ -9,11 +9,11 @@ import Foundation
 
 final class ContentViewModel: ObservableObject {
     let cells: [Cell] = [
-        Cell(cellTitle: "Codice fiscale", approxTime: "Approx. time 2-4 weeks", percentOfDone: 10),
-        Cell(cellTitle: "Accomodation", approxTime: "Approx. time 2-4 weeks", percentOfDone: 10),
-        Cell(cellTitle: "Permesso di Sogiorno", approxTime: "Approx. time 2-6 months", percentOfDone: 0),
-        Cell(cellTitle: "ADA contract", approxTime: "Approx. time 1-2 weeks", percentOfDone: 30),
-        Cell(cellTitle: "Transport in NA", approxTime: "Approx. time 1-2 weeks", percentOfDone: 10),
-        Cell(cellTitle: "Mobile operator", approxTime: "Approx. time 1-2 days", percentOfDone: 0)
+        Cell(cellTitle: "Codice fiscale", approxTime: "Approx. time 2-4 weeks", percentOfDone: 0.10),
+        Cell(cellTitle: "Accomodation", approxTime: "Approx. time 2-4 weeks", percentOfDone: 0.10),
+        Cell(cellTitle: "Permesso di Sogiorno", approxTime: "Approx. time 2-6 months", percentOfDone: 0.94),
+        Cell(cellTitle: "ADA contract", approxTime: "Approx. time 1-2 weeks", percentOfDone: 0.30),
+        Cell(cellTitle: "Transport in NA", approxTime: "Approx. time 1-2 weeks", percentOfDone: 0.10),
+        Cell(cellTitle: "Mobile operator", approxTime: "Approx. time 1-2 days", percentOfDone: 0.47)
     ]
 }

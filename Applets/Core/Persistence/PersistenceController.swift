3ec63983fdfe14b75e5a974b76d6347400b2ed10
egrishina <egrishina22@fed.idserve.net>
2023-02-20 10:57:11 +0100

    Fixed lint issues

--- Applets/Core/Persistence/PersistenceController.swift
+++ Applets/Core/Persistence/PersistenceController.swift
@@ -18,13 +18,17 @@ struct PersistenceController {
             task.id = UUID()
             task.title = "Task \(index)"
             task.information = """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             """
             task.timeFrame = 3
 
             let step = Step(context: viewContext)
             step.text = """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             """
             step.number = 1
             step.weight = 2

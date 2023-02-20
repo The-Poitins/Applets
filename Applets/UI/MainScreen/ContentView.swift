b112c88e2e7d1efd33ceebbebe486754fc765a96
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2023-02-20 12:51:01 +0100

    Initial commit

--- Applets/UI/MainScreen/ContentView.swift
+++ Applets/UI/MainScreen/ContentView.swift
@@ -9,20 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var contentVM = ContentViewModel()
+
+    @State private var isShowingProfilePage: Bool = false
+    @State private var isEditing: Bool = false
+
     var body: some View {
         NavigationView {
-            List(contentVM.tasks, id: \.self) { task in
-                NavigationLink {
-                    TaskView(taskName: task)
-                } label: {
-                    Text(task)
+            VStack {
+                HStack {
+                    Spacer()
+                    Button {
+                        isShowingProfilePage.toggle()
+                    } label: {
+                        Image(systemName: "person.circle")
+
+                    }
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                    .font(.system(size: 50))
                 }
+                Spacer()
+                List(contentVM.tasks, id: \.self) { task in
+                    NavigationLink {
+                        TaskView(taskName: task)
+                    } label: {
+                        Text(task)
+                    }
+                }
+                .listStyle(.plain)
+                .navigationTitle("Your tasks")
+
             }
-            .listStyle(.plain)
-            .navigationTitle("Your tasks")
+            .padding(20)
+
+
+            .sheet(isPresented: $isShowingProfilePage, onDismiss: { isEditing = false }) {
+                NavigationStack {
+                    UserProfileView(isEditing: $isEditing)
+                        .navigationTitle("User Profile")
+                        .toolbar {
+                            ToolbarItem(placement: .automatic) {
+                                Button {
+                                    isEditing.toggle()
+                                } label: {
+                                    Text("Edit")
+                                }
+                            }
+                        }
+                }
+
+            }
         }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

84de3b84a09f40bc01265c4b191b4ce3cf180e2d
alexnikxx <alexnikxx@users.noreply.github.com>
2023-02-20 13:00:17 +0100

    folder for modifiers

--- Applets/UI/MainScreen/CellView.swift
+++ Applets/UI/MainScreen/CellView.swift
@@ -50,28 +50,6 @@ struct CellView: View {
     }
 }
 
-struct FilledCell: ViewModifier {
-    var percentOfDone: Double
-    
-    func body(content: Content) -> some View {
-        content
-            .background(
-                GeometryReader { geometry in
-                    ZStack {
-                        ZStack(alignment: .leading) {
-                            Rectangle()
-                                .fill(Color.gray.opacity(0.40))
-                                .frame(width: geometry.size.width, height: geometry.size.height)
-                            Rectangle()
-                                .fill(Color.accentColor)
-                                .frame(width: CGFloat(percentOfDone) * geometry.size.width, height:  geometry.size.height)
-                        }
-                    }
-                }
-            )
-    }
-}
-
 
 struct Cell_Previews: PreviewProvider {
     static var previews: some View {

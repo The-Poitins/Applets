af2da7d4585b41e8b4a702d50d8200831f4b6625
alexnikxx <alexnikxx@users.noreply.github.com>
2023-02-21 10:18:18 +0100

    finished main view

--- Applets/UI/MainScreen/CellView.swift
+++ Applets/UI/MainScreen/CellView.swift
@@ -12,12 +12,14 @@ struct Cell: Identifiable {
     let cellTitle: String
     let approxTime: String
     let percentOfDone: Double
+    let isEnabled: Bool
     let color = Color.accentColor
     
-    init(cellTitle: String, approxTime: String, percentOfDone: Double) {
+    init(cellTitle: String, approxTime: String, percentOfDone: Double, isEnabled: Bool) {
         self.cellTitle = cellTitle
         self.approxTime = approxTime
         self.percentOfDone = percentOfDone
+        self.isEnabled = isEnabled
     }
 }
 
@@ -25,35 +27,49 @@ struct CellView: View {
     var cell: Cell
     
     var body: some View {
-        ZStack {
-            Text("\(Int(cell.percentOfDone * 100))%")
-                .frame(width: 370, height: 100, alignment: .trailing)
-                .font(.system(size: 100).bold())
-                .foregroundColor(.white.opacity(0.30))
-            
-            VStack(alignment: .leading) {
-                Text(cell.cellTitle)
-                    .font(.largeTitle.bold())
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 15)
-                    .foregroundColor(.black)
-                Text(cell.approxTime)
-                    .font(.subheadline)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 15)
-                    .foregroundColor(.black)
+            ZStack {
+                if cell.isEnabled {
+                    if cell.percentOfDone < 1 {
+                        HStack (alignment: .lastTextBaseline) {
+                            Text("\(Int(cell.percentOfDone * 100))")
+                                .font(.system(size: 100).bold().weight(.black))
+                                .padding(-10)
+                            Text("%")
+                                .font(.system(size: 40).bold().weight(.black))
+                                .padding(5)
+                        }
+                        .frame(width: 370, height: 100, alignment: .trailing)
+                        .foregroundColor(.white.opacity(0.30))
+                    } else {
+                        Text("DONE")
+                            .font(.system(size: 120).bold().weight(.black))
+                            .foregroundColor(.white.opacity(0.30))
+                    }
+                }
+                
+                VStack(alignment: .leading) {
+                    Text(cell.cellTitle)
+                        .font(.largeTitle.bold())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 15)
+                        .foregroundColor(cell.isEnabled ? .black : .gray)
+                    Text(cell.approxTime)
+                        .font(.subheadline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 15)
+                        .foregroundColor(cell.isEnabled ? .black : .gray)
+                }
             }
-        }
-        .frame(width: 370, height: 100)
-        .modifier(FilledCell(percentOfDone: cell.percentOfDone))
-        .cornerRadius(20)
+            .frame(width: 370, height: 100)
+            .modifier(FilledCell(percentOfDone: cell.percentOfDone))
+            .cornerRadius(20)
     }
 }
 
 
 struct Cell_Previews: PreviewProvider {
     static var previews: some View {
-        CellView(cell: Cell(cellTitle: "10", approxTime: "10", percentOfDone: 0.20))
+        CellView(cell: Cell(cellTitle: "10", approxTime: "10", percentOfDone: 0, isEnabled: false))
     }
 }
 

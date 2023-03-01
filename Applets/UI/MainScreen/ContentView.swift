a24d38ffc2b4f488e1b96a78c8d623108483921c
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2023-03-01 11:21:09 +0100

    Fix conflicts with onboarding2

--- Applets/UI/MainScreen/ContentView.swift
+++ Applets/UI/MainScreen/ContentView.swift
@@ -8,60 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-
     @StateObject var contentVM = ContentViewModel()
 
+    @State private var firstRun = true
     @State private var isShowingProfilePage = false
     @State private var isEditing = false
 
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                ForEach(contentVM.goals, id: \.id) { goal in
-                    if goal.isEnabled {
-                        NavigationLink {
-                            TaskView(goal: goal)
-                                .navigationTitle(goal.title ?? "")
-                                .navigationBarTitleDisplayMode(.inline)
-                        } label: {
+        if firstRun {
+            OnBoarding(firstRun: $firstRun)
+        } else {
+            NavigationStack {
+                ScrollView {
+                    ForEach(contentVM.goals, id: \.id) { goal in
+                        if goal.isEnabled {
+                            NavigationLink {
+                                TaskView(goal: goal)
+                                    .navigationTitle(goal.title ?? "")
+                                    .navigationBarTitleDisplayMode(.inline)
+                            } label: {
+                                CellView(model: goal)
+                                    .padding(.horizontal, 16)
+                                    .padding(.bottom)
+                            }
+                        } else {
                             CellView(model: goal)
                                 .padding(.horizontal, 16)
                                 .padding(.bottom)
                         }
-                    } else {
-                        CellView(model: goal)
-                            .padding(.horizontal, 16)
-                            .padding(.bottom)
                     }
                 }
-            }
-            .padding(.top, 8)
-            .navigationTitle("Your tasks")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color("yellowColor").opacity(0.10))
-            .toolbar {
-                ToolbarItem {
-                    Button {
-                        isShowingProfilePage.toggle()
-                    } label: {
-                        Image(systemName: "person.circle")
+                .padding(.top, 8)
+                .navigationTitle("Your tasks")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color("yellowColor").opacity(0.10))
+                .toolbar {
+                    ToolbarItem {
+                        Button {
+                            isShowingProfilePage.toggle()
+                        } label: {
+                            Image(systemName: "person.circle")
+                        }
+                        .font(.title)
                     }
-                    .font(.title)
                 }
-            }
-            .sheet(isPresented: $isShowingProfilePage, onDismiss: { isEditing = false }) {
-                NavigationStack {
-                    UserProfileView(isEditing: $isEditing)
-                        .navigationTitle("User Profile")
-                        .toolbar {
-                            ToolbarItem(placement: .automatic) {
-                                Button {
-                                    isEditing.toggle()
-                                } label: {
-                                    Text("Edit")
+                .sheet(isPresented: $isShowingProfilePage, onDismiss: { isEditing = false }) {
+                    NavigationStack {
+                        UserProfileView(isEditing: $isEditing)
+                            .navigationTitle("User Profile")
+                            .toolbar {
+                                ToolbarItem(placement: .automatic) {
+                                    Button {
+                                        isEditing.toggle()
+                                    } label: {
+                                        Text("Edit")
+                                    }
                                 }
                             }
-                        }
+                    }
                 }
             }
         }

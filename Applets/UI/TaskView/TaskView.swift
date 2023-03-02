208d91580f15d0f3b4d01ad58ecbc36fa2f01ced
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2023-03-02 11:45:34 +0100

    Add contentShape(Circle()) to the buttons on the step view. Previously they were hard to tap and often brought up the modal.

--- Applets/UI/TaskView/TaskView.swift
+++ Applets/UI/TaskView/TaskView.swift
@@ -24,7 +24,7 @@ struct TaskView: View {
                     .foregroundColor(.clear)
                     .cornerRadius(20)
                     .shadow(color: .black.opacity(0.1), radius: 20, y: 2)
-                Text("\(viewModel.goal.percentOfDone)% has done")
+                Text("\(viewModel.goal.percentOfDone)% done")
                     .frame(maxWidth: .infinity)
             }
             .padding(.horizontal)
